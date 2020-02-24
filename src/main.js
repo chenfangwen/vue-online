f381e25f3005039d0911082ab3e194b742e2f468
chenfangwen <2325189133@qq.com>
2020-02-24 20:57:06 +0800

    表单

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import iView from 'iview' //UI框架
+import 'iview/dist/styles/iview.css';
 
+Vue.use(iView)
 Vue.config.productionTip = false
 
 new Vue({
